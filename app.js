f5d29994025d4cd1290d8afa5dc9d595fe019bd7
Ravi <ravimahendrapratap178@gmail.com>
2024-07-08 20:20:36 +0530

    added route

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express= require('express');
 const app= express();
 
+app.get('/', (req, res)=>{
+    res.send('Application Running');
+})
+
 app.listen(3000);
 console.log('app is running on port 3000');
