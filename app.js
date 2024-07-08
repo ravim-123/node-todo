9a53092fc19418becd8cdb14cc8928de88d11064
Ravi <ravimahendrapratap178@gmail.com>
2024-07-08 22:13:39 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app= express();
 
 app.get('/', (req, res)=>{
-    res.send('Application Running');
+    res.send('Application Running Using CI CD');
 })
 
 app.listen(3000);
